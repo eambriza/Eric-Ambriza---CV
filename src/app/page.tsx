1885d3b78615256aeed77a2964446833d8b1e16c
eambriza <63431106+eambriza@users.noreply.github.com>
2026-04-10 10:43:43 -0700

    Embed static CV design using remote iframe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 export default function Home() {
   return (
-    <div>Hello world</div>
+    <iframe
+      src="https://raw.githubusercontent.com/eambriza/Eric-Ambriza---CV/main/public/eric-ambriza-cv-final_3.html"
+      style={{ width: '100%', height: '100vh', border: 'none' }}
+      title="Eric Ambriza CV"
+    />
   );
 }
