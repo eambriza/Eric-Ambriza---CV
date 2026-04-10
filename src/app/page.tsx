3f028d2487e4b18eb7789b3073a4b62b624f9245
eambriza <63431106+eambriza@users.noreply.github.com>
2026-04-10 10:35:22 -0700

    Test root page to debug 404

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,5 @@
-'use client';
-
 export default function Home() {
   return (
-    <iframe
-      src="/eric-ambriza-cv-final_3.html"
-      style={{ width: '100%', height: '100vh', border: 'none' }}
-      title="Eric Ambriza CV"
-    />
+    <div>Hello world</div>
   );
 }
